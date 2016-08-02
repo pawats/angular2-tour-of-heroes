795490fca57c0acf44b2e407101784a1d71a99b2
Pawat Supaongprapa <pawatsupaongprapa@Administrators-MacBook-Pro.local>
2016-08-02 17:41:34 +0700

    Add routing

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,21 +1,38 @@
-import { Component, Input } from '@angular/core';
+// import { Component, Input } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Hero } from './hero';
+import { ActivatedRoute } from '@angular/router';
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'my-hero-detail',
-  template: `
-	<div *ngIf="hero">
-		<h2>{{hero.name}} details!</h2>
-		<div><label>id: </label>{{hero.id}}</div>
-		<div>
-			<label>name: </label>
-			<input [(ngModel)]="hero.name" placeholder="name">
-		</div>
-	</div>
-  `
+  templateUrl: 'app/hero-detail.component.html',
+  styleUrls: ['app/hero-detail.component.css']
 })
 
-export class HeroDetailComponent {
-	@Input()
+export class HeroDetailComponent implements OnInit, OnDestroy {
 	hero: Hero;
+	sub: any;
+
+	constructor(
+		private heroService: HeroService,
+		private route: ActivatedRoute) {
+		}
+
+	ngOnInit() {
+		this.sub = this.route.params.subscribe(params => {
+		let id = +params['id'];
+		this.heroService.getHero(id)
+			.then(hero => this.hero = hero);
+		});
+	}
+
+	ngOnDestroy() {
+		this.sub.unsubscribe();
+	}
+
+	goBack() {
+		window.history.back();
+	}
+
 }
